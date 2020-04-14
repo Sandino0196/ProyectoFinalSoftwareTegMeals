1d7648b154ea5c489621c9a0b2a08530dda5ef03
Sandino <sandinorodrigo@gmail.com>
2020-04-14 15:53:23 -0600

    CSS

--- frnlencastore/src/componentes/Pages/Public/Home/Home.js
+++ frnlencastore/src/componentes/Pages/Public/Home/Home.js
@@ -3,7 +3,7 @@ import Page from '../../Page';
 
 export default ({auth})=>{
   return (
-    <Page pageTitle="Home Page" auth={auth}>
+    <Page pageTitle="TEGMEALS" auth={auth}>
       <p>Bienvenidos a TegMeals</p>
       <p>#QuedateEnCasa</p>
     </Page>
